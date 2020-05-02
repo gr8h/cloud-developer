69bbefc4365fb5b6af4da71121fcef19c7f643c8
Hesham Shabanah <gr8h@Heshams-MBP.fritz.box>
2020-05-02 11:56:41 +0200

    Implement filteredimage endpoint

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -37,6 +37,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
+  // Filter Image
+  app.get( "/filteredimage/", async ( req, res, next ) => {
+    const { image_url } = req.query;
+
+    // validate the image_url query
+    if(!image_url) {
+      return res.status(400).send('The image url is missing.');
+    }
+
+    // call filterImageFromURL(image_url) to filter the image
+    const filteredpath = await filterImageFromURL(image_url);
+    res.sendFile(filteredpath);
+
+    // deletes any files on the server on finish of the response
+    res.on('finish', () => {
+      deleteLocalFiles([filteredpath]);
+    } );
+
+  } );
 
   // Start the Server
   app.listen( port, () => {
